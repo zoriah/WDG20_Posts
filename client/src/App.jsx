d26d8400371c657d25611941dc5a1596e16cd722
Smirnov <smirnov.mykyta@gmail.com>
2025-01-29 10:58:04 +0100

    add Routers and updated MainLayout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import MainLayout from './Layouts/MainLayout';
+import {CardList} from "./components/CardList.jsx";
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<MainLayout />}>
-          <Route index element={<h2>HOME</h2>} />
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<MainLayout/>}>
+                    <Route index element={<CardList/>}/>
+                    {/*<Route path="postcreate" element={<PostCreate/>}/>*/}
+                    {/*<Route path="postcard/:postId" element={<PostDetails/>}/>*/}
 
-          <Route path='*' element={<h2>Not Found</h2>} />
-        </Route>
-      </Routes>
-    </>
-  );
+                    <Route path='*' element={<h2>Not Found</h2>}/>
+                </Route>
+            </Routes>
+        </>
+    );
 };
 
 export default App;
