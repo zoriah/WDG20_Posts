e6409842e055a27299d8b2e96f322bade60175bb
ilonavich <ilonavich@yahoo.de>
2025-01-30 14:34:26 +0100

    deleted unnecessary comments and files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api/posts": {
-  //       target: "http://localhost:8080/posts",
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
 });
